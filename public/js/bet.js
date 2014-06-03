e5468e034064a6417087f565fff6218096a6637a
Wei Huang <hweicdl@cn.ibm.com>
2014-06-03 09:27:38 +0800

    popup delay 效果

--- public/js/bet.js
+++ public/js/bet.js
@@ -1,3 +1,14 @@
+function popupMsg(css, msg, matchid) {
+  $("#popup").addClass(css);
+  $("#popup").empty().append(msg);
+  $("#popup").show();
+  $("#popup").delay(2500).fadeOut(1000, function() {
+    console.log("callback fadeout");
+    $("#btn"+matchid).removeAttr('disabled');
+    $("#popup").removeClass(css);
+  });
+}
+
 function bet(matchid) {
   var rowno = matchid - 1;
   // console.log('bet() invoked...');
@@ -5,22 +16,23 @@ function bet(matchid) {
   var score2 = $("#score2"+matchid).text();
   var matchTime = $("#time"+matchid).attr("value");
 
-  $.post('/bet/vote', {
-    score1: score1,
-    score2: score2,
-    rowno: rowno,
-    matchTime: matchTime},
-    function(data) {
-      var css = data.split(",")[0];
-      var msg = data.split(",")[1]
-      $("#popup").addClass(css);
-      $("#popup").empty().append(msg);
-      $("#popup").show();
-      $("#popup").fadeOut(3000, function() {
-        console.log("callback fadeout");
-        $("#popup").removeClass(css);
-      });
-  });
+  $("#btn"+matchid).attr('disabled', true);
+
+  if (isNaN(score1) || isNaN(score2)) {
+    popupMsg('alert-danger', '亲，竞猜比分只能为数字且不含空格', matchid);
+  }
+  else {
+    $.post('/bet/vote', {
+      score1: score1,
+      score2: score2,
+      rowno: rowno,
+      matchTime: matchTime},
+      function(data) {
+        var css = data.split(",")[0];
+        var msg = data.split(",")[1];
+        popupMsg(css, msg, matchid);
+    });    
+  }
 };
 
 function getFocus(id) {
