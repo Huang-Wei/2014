ac1632537d5d1efb85fbe93b623db0cb80714e91
Wei Huang <hweicdl@cn.ibm.com>
2014-06-16 17:52:21 +0800

    minor changes

--- public/js/bet.js
+++ public/js/bet.js
@@ -7,15 +7,34 @@ function popupMsg(css, msg, matchid) {
     $("#subOne"+matchid).removeAttr('disabled');
     $("#popup").removeClass(css);
   });
-}
+};
+
+// $('.score').each(function() {
+//   var elem = $(this);
+//   // Save current value of element
+//   elem.data('oldVal', elem.val());
+//   // Look for changes in the value
+//   elem.bind("propertychange keyup input paste", function(event){
+//   // If value has changed...
+//     if (elem.data('oldVal') != elem.val()) {
+//       // Updated stored value
+//       elem.data('oldVal', elem.val());
+//       console.log('xxx');
+//     }
+//   });
+// });
+
+// $('.score').propertychange(function() {
+//   console.log('xxx');
+// })
 
 $('button[id^="subOne"]').click(function() {
-  var inputs = $(this).parent().parent().find("input");
+  var inputs = $(this).parent().parent().find("input[type='text']");
+  var score1 = inputs[0].value.trim();
+  var score2 = inputs[1].value.trim();
   var matchid = $(this).parent().next().text();
   matchid = parseInt(matchid.substring(1, matchid.length-1)); //比赛id
   var matchTime = $("#time"+matchid).attr("value");
-  var score1 = inputs[0].value.trim();
-  var score2 = inputs[1].value.trim();
 
   $(this).attr('disabled', true);
 
@@ -47,13 +66,14 @@ $(".score").blur(function() {
 
 $(function() {
   // console.log("load() invoked...");
-  var rowCount = $("#myTable tr").length;
+  var rowCount = $('button[id^="subOne"]').length;
   var thisTime = new Date();
   console.log("table rowCount : " + rowCount);
   for (var i = 1; i <= rowCount; i++) {
     var matchTime = new Date(parseInt($("#time"+i).attr("value")));
     if (thisTime > matchTime) {
       $("#subOne"+i).attr("disabled", true);
+      $("#subOne"+i).parent().parent().find("input").attr('disabled', true);
       // $("#score1"+i).attr("contenteditable", false);
       // $("#score2"+i).attr("contenteditable", false);
     }
